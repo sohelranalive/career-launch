3e830f0bb1961f5343f86432a26d9ea9a29513f4
sohelranalive <sohel.cse37@gmail.com>
2023-04-12 13:17:44 +0600

    filter added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import JobReview from '../JobReview/JobReview';
 
 const AppliedJobs = () => {
 
-    const appliedJobs = useLoaderData()
+    const appliedJobsData = useLoaderData()
+
+
+    const [appliedJobs, setAppliedJobs] = useState(appliedJobsData)
+
+
+    const handleFilterRemoteJobs = () => {
+        const remoteJobs = appliedJobsData.filter(aj => aj.remoteOrOnsite === 'Remote')
+        setAppliedJobs(remoteJobs)
+    }
+
+
+    const handleFilterOnsiteJobs = () => {
+        const onsiteJobs = appliedJobsData.filter(aj => aj.remoteOrOnsite === 'Onsite')
+        setAppliedJobs(onsiteJobs)
+    }
+
 
     return (
         <div>
             <h1 className='text-3xl text-center custom-bg p-10'>Applied Job</h1>
             <div className='custom-area mt-20 mb-20'>
-                <button>Filter</button>
+
+                <div className='flex gap-2 justify-end items-center'>
+                    <small>Filter</small>
+                    <button onClick={handleFilterRemoteJobs} className='border rounded-md px-3 py-1 border-purple-900 hover:bg-indigo-200'>Remote</button>
+                    <button onClick={handleFilterOnsiteJobs} className='border rounded-md px-3 py-1 border-purple-900 hover:bg-indigo-200'>Onsite</button>
+                </div>
+
                 <br />
                 <br />
                 {
-                    appliedJobs.map(job => <JobReview job={job}></JobReview>)
+                    appliedJobs.map(job => <JobReview key={job.id} job={job}></JobReview>)
                 }
             </div>
         </div>
